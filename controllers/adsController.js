f200604dd888b040e035db52f541efd8ef21e3f0
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2025-03-30 12:09:22 +0600

    ads section

--- controllers/adsController.js
+++ controllers/adsController.js
@@ -1,32 +1,48 @@
 const Advertisement = require("../models/advertisement");
 
 const addAds = (req, res, next) => {
-  const { type, url } = req.body;
-  if (type == "" || url == "") {
-    res.status(500).json({
-      error: "Need to fill all necessary fields.",
-    });
-  } else {
-    try {
-      const ads = new Advertisement(type, url);
-      ads.save();
+    const { type, url } = req.body;
+    if (type == "" || url == "") {
+        res.status(500).json({
+            error: "Need to fill all necessary fields.",
+        });
+    } else {
+        try {
+            const ads = new Advertisement(type, url);
+            ads.save();
 
-      res.status(201).json({
-        msg: "Ads added successfully!",
-      });
-    } catch (err) {
-      res.status(500).json({
-        msg: "Can't add Ads!",
-      });
+            res.status(201).json({
+                msg: "Ads added successfully!",
+            });
+        } catch (err) {
+            res.status(500).json({
+                msg: "Can't add Ads!",
+            });
+        }
     }
-  }
 };
 
 const getAds = (req, res, next) => {
-  Advertisement.getAds((data) => res.status(200).json(data));
+    Advertisement.getAds((data) => res.status(200).json(data));
+};
+
+const deleteAds = (req, res, next) => {
+    try {
+        const ads = new Advertisement("", "");
+        ads.save();
+
+        res.status(201).json({
+            msg: "Ads deleted successfully!",
+        });
+    } catch (err) {
+        res.status(500).json({
+            msg: "Can't delete Ads!",
+        });
+    }
 };
 
 module.exports = {
-  getAds,
-  addAds,
+    getAds,
+    addAds,
+    deleteAds,
 };
--- routes/adsRouter.js
+++ routes/adsRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const authurization = require("../middlewares/authorization");
-const { addAds, getAds } = require("../controllers/adsController");
+const { addAds, getAds, deleteAds } = require("../controllers/adsController");
 const router = express.Router();
 
 router.post("/add", authurization, addAds);
 router.get("/", getAds);
+router.post("/delete", deleteAds);
 
 module.exports = router;
